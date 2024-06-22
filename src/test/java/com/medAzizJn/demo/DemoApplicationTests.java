6db598a4d68598f8c77e72f182c0929e2519a9a9
TeamDev2002 <teamdev172002@gmail.com>
2024-06-22 06:28:00 +0100

    initializing spring boot project

---
+++ src/test/java/com/medAzizJn/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medAzizJn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
