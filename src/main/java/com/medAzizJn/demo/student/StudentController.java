6644ef2870e009becd1ef93c6d2bcc951f4739dc
TeamDev2002 <teamdev172002@gmail.com>
2024-06-22 07:06:33 +0100

    Building First API

---
+++ src/main/java/com/medAzizJn/demo/student/StudentController.java
@@ -0,0 +1,18 @@
+package com.medAzizJn.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findAllStudent(){
+        return List.of("Med Aziz" , "Jnayah");
+    }
+}
+
